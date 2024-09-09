1a641d56fd7e5ec0349485fc325395ec99f5c880
Vladimir-Tur <vovanru2033@mail.ru>
2024-09-09 15:06:46 +0400

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name = 'Vladimir'
+print(name, type(name))
+age = 23
+print(age, type(age))
+new = 1
+is_student = (1==1)
+print(is_student, type(is_student))
+print('Name: ', name)
+print('Age: ', age)
+print('New Age: ', age + new)
+print("Is Student: ", is_student)
+
+
