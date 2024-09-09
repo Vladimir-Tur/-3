44c9a12cf4644e28e28ce91d2c58b8378194abd2
Vladimir-Tur <vovanru2033@mail.ru>
2024-09-09 20:43:54 +0400

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,12 +2,11 @@ name = 'Vladimir'
 print(name, type(name))
 age = 23
 print(age, type(age))
-new = 1
-is_student = (1==1)
+is_student = (True)
 print(is_student, type(is_student))
 print('Name: ', name)
 print('Age: ', age)
-print('New Age: ', age + new)
+print('New Age: ', age + 1)
 print("Is Student: ", is_student)
 
 
